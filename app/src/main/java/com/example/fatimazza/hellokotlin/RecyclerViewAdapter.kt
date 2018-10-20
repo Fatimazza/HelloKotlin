f572528906a5338ef85dd8b679a01c6f994d3790
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-20 17:59:55 +0700

    Adapter - Add new Recyclerview Adapter

---
+++ app/src/main/java/com/example/fatimazza/hellokotlin/RecyclerViewAdapter.kt
@@ -0,0 +1,12 @@
+package com.example.fatimazza.hellokotlin
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+class RecyclerViewAdapter(private val context: Context, private val item:List<Item>) {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        
+    }
+}
