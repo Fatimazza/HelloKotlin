8d25183e2698d685afdf7ec81facbc8e70c63e7a
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-20 21:29:46 +0700

    Model - Implement Parcelable

--- app/src/main/java/com/example/fatimazza/hellokotlin/Item.kt
+++ app/src/main/java/com/example/fatimazza/hellokotlin/Item.kt
@@ -1,3 +1,7 @@
 package com.example.fatimazza.hellokotlin
 
-data class Item (val name: String?, val image:Int?)
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Item(val name: String?, val image:Int?) : Parcelable
