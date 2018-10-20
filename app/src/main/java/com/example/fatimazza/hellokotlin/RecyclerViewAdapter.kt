36fc40a256c044c035be55638a1d27437fca1602
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-20 18:11:20 +0700

    Adapter - Fix View Holder, Fix Implemented Function Body, Add Picasso to load Image

--- app/src/main/java/com/example/fatimazza/hellokotlin/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/fatimazza/hellokotlin/RecyclerViewAdapter.kt
@@ -2,25 +2,34 @@ package com.example.fatimazza.hellokotlin
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.squareup.picasso.Picasso
 
-class RecyclerViewAdapter(private val context: Context, private val item:List<Item>)
+class RecyclerViewAdapter(private val context: Context, private val items:List<Item>)
     : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+            ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_list, parent, false))
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getItemCount(): Int = items.size
 
-    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindItems(items[position])
     }
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private val name = view.findViewById<TextView>(R.id.name)
+        private val image = view.findViewById<ImageView>(R.id.image)
+
+        fun bindItems(items: Item) {
+            name.text = items.name
+            items.image?.let { Picasso.get().load(it).into(image) }
+        }
         
     }
 }
