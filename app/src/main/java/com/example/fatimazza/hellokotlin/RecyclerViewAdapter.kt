cfd71caeb8ce3a196b814d7f0e0182692b875471
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-20 18:02:40 +0700

    Adapter - Implement Adapter ViewHolder

--- app/src/main/java/com/example/fatimazza/hellokotlin/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/fatimazza/hellokotlin/RecyclerViewAdapter.kt
@@ -3,8 +3,22 @@ package com.example.fatimazza.hellokotlin
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.view.ViewGroup
 
-class RecyclerViewAdapter(private val context: Context, private val item:List<Item>) {
+class RecyclerViewAdapter(private val context: Context, private val item:List<Item>)
+    : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         
