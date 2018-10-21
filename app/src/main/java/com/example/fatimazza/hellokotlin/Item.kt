3d9ced47888f7e339734080318ac89503cd8b5dc
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-21 12:48:10 +0700

    Model, Main - Add Description, Sent Intent Description

--- app/src/main/java/com/example/fatimazza/hellokotlin/Item.kt
+++ app/src/main/java/com/example/fatimazza/hellokotlin/Item.kt
@@ -4,4 +4,4 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class Item(val name: String?, val image:Int?) : Parcelable
+data class Item(val name: String?, val desc: String?, val image:Int?) : Parcelable
--- app/src/main/java/com/example/fatimazza/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/fatimazza/hellokotlin/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.fatimazza.hellokotlin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
 
@@ -19,22 +18,19 @@ class MainActivity : AppCompatActivity() {
 
         club_list.layoutManager = LinearLayoutManager(this)
         club_list.adapter = RecyclerViewAdapter(this, items) {
-
             startActivity<DetailActivity>(
-                    "name" to "${it.name}", "desc" to "${it.name}")
-
-            val toast = Toast.makeText(applicationContext, it.name, Toast.LENGTH_SHORT)
-            toast.show()
+                    "name" to "${it.name}", "desc" to "${it.desc}")
         }
     }
 
     private fun initData() {
         val name = resources.getStringArray(R.array.club_name)
+        val desc = resources.getStringArray(R.array.club_desc)
         val image = resources.obtainTypedArray(R.array.club_image)
 
         items.clear()
         for (i in name.indices){
-            items.add(Item(name[i], image.getResourceId(i, 0)))
+            items.add(Item(name[i], desc[i], image.getResourceId(i, 0)))
         }
 
         // Recycle the typed array
