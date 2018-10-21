c720cea8b2770e5df54bb0715390b8b86b768dbc
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-21 09:56:33 +0700

    Layout, Main - Add Text and Button in Anko Activity

--- app/src/main/java/com/example/fatimazza/hellokotlin/AnkoCommonActivity.kt
+++ app/src/main/java/com/example/fatimazza/hellokotlin/AnkoCommonActivity.kt
@@ -1,7 +1,10 @@
 package com.example.fatimazza.hellokotlin
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import com.example.fatimazza.hellokotlin.R.color.colorAccent
 import org.jetbrains.anko.*
 
 class AnkoCommonActivity : AppCompatActivity() {
@@ -15,6 +18,52 @@ class AnkoCommonActivity : AppCompatActivity() {
         override fun createView(ui: AnkoContext<AnkoCommonActivity>) = with(ui) {
             verticalLayout {
                 padding = dip(16)
+
+                val name = editText{
+                    hint = "What's your name?"
+                }
+
+                button("Say Hello") {
+                    backgroundColor = ContextCompat.getColor(context, colorAccent)
+                    textColor = Color.WHITE
+                }.lparams(width = matchParent) {
+                    topMargin = dip(5)
+                }
+
+                button("Show Alert") {
+                    backgroundColor = ContextCompat.getColor(context, colorAccent)
+                    textColor = Color.WHITE
+                }.lparams(width = matchParent){
+                    topPadding = dip(5)
+                }
+
+                button("Show Selector") {
+                    backgroundColor = ContextCompat.getColor(context, colorAccent)
+                    textColor = Color.WHITE
+                }.lparams(width = matchParent) {
+                    topPadding = dip(5)
+                }
+
+                button("Show Snackbar") {
+                    backgroundColor = ContextCompat.getColor(context, colorAccent)
+                    textColor = Color.WHITE
+                }.lparams(width = matchParent) {
+                    topPadding = dip(5)
+                }
+
+                button("Show Progress Bar") {
+                    backgroundColor = ContextCompat.getColor(context, colorAccent)
+                    textColor = Color.WHITE
+                }.lparams(width = matchParent) {
+                    topPadding = dip(5)
+                }
+
+                button("Go To Other Activity") {
+                    backgroundColor = ContextCompat.getColor(context, colorAccent)
+                    textColor = Color.WHITE
+                }.lparams(width = matchParent) {
+                    topPadding = dip(5)
+                }
             }
         }
     }
