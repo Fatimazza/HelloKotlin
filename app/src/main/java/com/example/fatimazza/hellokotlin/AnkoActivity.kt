2c2864160b9bd1db1873022e946a522c374aae11
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-21 09:10:07 +0700

    Main - Add Listener in Anko, Practice Anko Common functions for Intent

--- app/src/main/java/com/example/fatimazza/hellokotlin/AnkoActivity.kt
+++ app/src/main/java/com/example/fatimazza/hellokotlin/AnkoActivity.kt
@@ -30,6 +30,9 @@ class AnkoActivity : AppCompatActivity() {
                         }.show()
                     }
                 }
+                button("Jump to Other Activity") {
+                    onClick { startActivity<MainActivity>() }
+                }
             }
         }
     }
