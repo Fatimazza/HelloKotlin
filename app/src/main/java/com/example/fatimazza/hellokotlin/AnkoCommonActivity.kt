5fc672721c13d3d7cb7ce813c7b81361c28b01cc
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-21 09:39:54 +0700

    Layout, Main - Refactor Class Name, Make activity as first launch

--- app/src/main/java/com/example/fatimazza/hellokotlin/AnkoCommonActivity.kt
+++ app/src/main/java/com/example/fatimazza/hellokotlin/AnkoCommonActivity.kt
@@ -4,15 +4,15 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import org.jetbrains.anko.*
 
-class AnkoListActivity : AppCompatActivity() {
+class AnkoCommonActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        AnkoListActivityUI().setContentView(this)
+        AnkoCommonActivityUI().setContentView(this)
     }
 
-    class AnkoListActivityUI : AnkoComponent<AnkoListActivity> {
-        override fun createView(ui: AnkoContext<AnkoListActivity>) = with(ui) {
+    class AnkoCommonActivityUI : AnkoComponent<AnkoCommonActivity> {
+        override fun createView(ui: AnkoContext<AnkoCommonActivity>) = with(ui) {
             verticalLayout {
                 padding = dip(16)
             }
