c35a15a9ef3d3985cf33cac096d81d4268e2f3b6
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-21 11:32:44 +0700

    Detail, Layout - Add Image and Text using Anko

--- app/src/main/java/com/example/fatimazza/hellokotlin/DetailActivity.kt
+++ app/src/main/java/com/example/fatimazza/hellokotlin/DetailActivity.kt
@@ -2,6 +2,7 @@ package com.example.fatimazza.hellokotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity.CENTER_HORIZONTAL
 import org.jetbrains.anko.*
 
 class DetailActivity : AppCompatActivity() {
@@ -14,7 +15,26 @@ class DetailActivity : AppCompatActivity() {
     class DetailActivityUI : AnkoComponent<DetailActivity> {
         override fun createView(ui: AnkoContext<DetailActivity>) = with(ui) {
             verticalLayout {
+                gravity = CENTER_HORIZONTAL
                 padding = dip(16)
+
+                imageView(R.drawable.img_barca)
+                        .lparams(width = dip(80), height = dip(80))
+
+                val textName = textView()
+                        .lparams(wrapContent) {
+                            topMargin = dip(5)
+                        }
+                textName.text = "FC Name"
+
+                val textDesc = textView()
+                        .lparams(matchParent) {
+                            topMargin = dip(10)
+                        }
+                textDesc.text = "Long Description Goes Here Long Description Goes Here " +
+                        "Long Description Goes Here Long Description Goes Here " +
+                        "Long Description Goes Here Long Description Goes Here " +
+                        "Long Description Goes Here Long Description Goes Here "
             }
         }
     }
