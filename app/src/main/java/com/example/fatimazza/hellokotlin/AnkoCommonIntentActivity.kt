fee1a117bf44bd2c33e62d73e86aaf0e0cfb258c
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-21 10:40:16 +0700

    Main - Add new Anko Activity

---
+++ app/src/main/java/com/example/fatimazza/hellokotlin/AnkoCommonIntentActivity.kt
@@ -0,0 +1,13 @@
+package com.example.fatimazza.hellokotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AnkoCommonIntentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
